ddd09de86974ee48e873b2cd591d3b1b60aa0e09
Sharp-rookie <1842604700@qq.com>
2023-01-08 14:07:42 +0000

    add FHN Dataset

--- FHNExperiment/Data/1_creating_figures.py
+++ FHNExperiment/Data/1_creating_figures.py
@@ -14,7 +14,7 @@ font = {'weight':'normal', 'size':16}
 plt.rc('font', **font)
 
 
-data = np.load("Data/origin/lattice_boltzmann.npz")['data']
+data = np.load("Data/origin/lattice_boltzmann.npz", allow_pickle=True)
 rho_act_all = np.array(data["rho_act_all"])
 rho_in_all = np.array(data["rho_in_all"])
 x = np.array(data["x"])
@@ -26,9 +26,8 @@ print(np.shape(rho_act_all))
 
 labels = ["Test", "Train", "Train", "Train", "Val", "Val"]
 fig, (ax1, ax2) = plt.subplots(1,2,figsize=(15,5))
-# fig.suptitle('Initial conditions')
 for ic in range(np.shape(rho_act_all)[0]):
-    ax1.plot(x, rho_act_all[ic,0,:], label="{:}".format(labels[ic]), linewidth=2)
+    ax1.plot(x, rho_act_all[ic,0,:], label="trace{:}".format(labels[ic]), linewidth=2)
 
 ax1.set_ylabel(r"$u(x,t=0)$")
 ax1.set_xlabel(r"$x$")
@@ -36,8 +35,7 @@ ax1.set_xlim([np.min(x), np.max(x)])
 ax1.set_title("Activator")
 
 for ic in range(np.shape(rho_in_all)[0]):
-    ax2.plot(x, rho_in_all[ic,0,:], label="{:}".format(labels[ic]), linewidth=2)
-
+    ax2.plot(x, rho_in_all[ic,0,:], label="trace{:}".format(labels[ic]), linewidth=2)
 
 ax2.set_ylabel(r"$v(x,t=0)$")
 ax2.set_xlabel(r"$x$")
@@ -47,8 +45,9 @@ ax2.set_xlim([np.min(x), np.max(x)])
 plt.tight_layout()
 
 os.makedirs("Data/origin/figures/", exist_ok=True)
-plt.savefig("Data/origin/figures/Plot_initial_conditions.pdf", bbox_inches="tight")
+plt.savefig("Data/origin/figures/initial_conditions.jpg", bbox_inches="tight", dpi=300)
 plt.close()
+
 
 for ic in range(len(rho_act_all)):
 
@@ -56,9 +55,16 @@ for ic in range(len(rho_act_all)):
     N_end = int(T_end / dt_data)
 
     rho_act = rho_act_all[ic]
+    rho_in = rho_in_all[ic]
     t_vec = t_vec_all[ic]
 
     subsample_time = 10
+    
+    os.makedirs("Data/origin/figures/{:}/".format(ic), exist_ok=True)
+    
+    ############
+    # Activator
+    ############
 
     X = x
     Y = t_vec[:N_end]
@@ -68,7 +74,6 @@ for ic in range(len(rho_act_all)):
 
     Z = rho_act[:N_end]
     Z = Z[::subsample_time]
-
 
     fig = plt.figure(figsize=(12,10))
     ax = fig.gca(projection='3d')
@@ -85,10 +90,8 @@ for ic in range(len(rho_act_all)):
     fig.colorbar(surf, orientation="horizontal")
     ax.invert_xaxis()
     ax.view_init(elev=34., azim=-48.)
-    plt.savefig("Data/origin/figures/Plot_surface_act_{:}.pdf".format(ic))
+    plt.savefig("Data/origin/figures/{:}/surface_act.jpg".format(ic), dpi=300)
     plt.close()
-
-
 
     fig = plt.figure()
     ax = fig.gca()
@@ -97,18 +100,13 @@ for ic in range(len(rho_act_all)):
     ax.set_xlabel(r"$x$")
     fig.colorbar(mp)
     plt.gca().set_rasterization_zorder(-1)
-    plt.savefig("Data/origin/figures/Plot_contourf_act_{:}.pdf".format(ic), bbox_inches="tight")
+    plt.savefig("Data/origin/figures/{:}/contourf_act.jpg".format(ic), bbox_inches="tight", dpi=300)
     plt.close()
 
-for ic in range(len(rho_in_all)):
-    T_end = 451
-    N_end = int(T_end / dt_data)
-
-    rho_in = rho_in_all[ic]
-    t_vec = t_vec_all[ic]
-
-    subsample_time = 10
-
+    ############
+    # Inhibitor
+    ############
+    
     X = x
     Y = t_vec[:N_end]
     Y = Y[::subsample_time]
@@ -117,8 +115,7 @@ for ic in range(len(rho_in_all)):
 
     Z = rho_in[:N_end]
     Z = Z[::subsample_time]
-
-
+    
     fig = plt.figure(figsize=(12,10))
     ax = fig.gca(projection='3d')
     print(np.shape(X))
@@ -134,7 +131,7 @@ for ic in range(len(rho_in_all)):
     fig.colorbar(surf, orientation="horizontal")
     ax.invert_xaxis()
     ax.view_init(elev=34., azim=-48.)
-    plt.savefig("Data/origin/figures/Plot_surface_in_{:}.pdf".format(ic))
+    plt.savefig("Data/origin/figures/{:}/surface_in.jpg".format(ic), dpi=300)
     plt.close()
 
     fig = plt.figure()
@@ -143,7 +140,6 @@ for ic in range(len(rho_in_all)):
     ax.set_ylabel(r"$t$")
     ax.set_xlabel(r"$x$")
     fig.colorbar(mp)
-    # plt.savefig("Data/origin/figures/Plot_contourf_in_{:}.png".format(ic), bbox_inches="tight")
     plt.gca().set_rasterization_zorder(-1)
-    plt.savefig("Data/origin/figures/Plot_contourf_in_{:}.pdf".format(ic), bbox_inches="tight")
+    plt.savefig("Data/origin/figures/{:}/contourf_in.jpg".format(ic), bbox_inches="tight", dpi=300)
     plt.close()
